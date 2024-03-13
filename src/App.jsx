c9b89029e6bd999f4d4c23cb8890da1c5ddd9a0d
Excel oladipupo <shiloholadipupo@gamil.com>
2024-03-13 15:38:14 +0100

    completed final task

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,30 @@ import Price from "./components/Price";
 import Image from "./components/Image";
 
 function App() {
+  let name = "Rose";
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Image />
-        <Card.Body>
-          <Card.Title>
-            {" "}
-            <Name />{" "}
-          </Card.Title>
-          <Card.Text>
-            <Description />
-          </Card.Text>
-          <Button variant="primary">
-            <Price />
-          </Button>
-        </Card.Body>
-      </Card>
+      <div className="design">
+        <Card style={{ width: "18rem" }}>
+          <Image />
+          <Card.Body>
+            <Card.Title>
+              {" "}
+              <Name />{" "}
+            </Card.Title>
+            <Card.Text>
+              <Description />
+            </Card.Text>
+            <Button variant="primary">
+              <Price />
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <h1>Hello {name ? name : "there"} </h1>
+        {name && <Image />}
+      </div>
     </>
   );
 }
